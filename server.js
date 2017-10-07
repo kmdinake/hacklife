45907e46a48ed6d2d729e5e289273ae1c64d72d9
Keoagile Dinake <kmdinake@gmail.com>
2017-10-07 03:46:59 +0200

    fixed my previous merge conflicts

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.post('/registerNewUser', function (req, res) {
 			res.end();
 		}
 	});
-<<<<<<< HEAD
 });
 
 app.post('/retrieveDatasets', function(req, res){
@@ -66,59 +65,48 @@ app.post('/retrieveDatasets', function(req, res){
 		args: [sendPyReq]
 	};
 
-
 	PythonShell.run('/scripts/repository/getAllData.py', options, function (err, results) {
-		if (err)
-		{
+		if (err){
 			console.log("Error failed to get datasets: " + err);
-=======
-})
 
-//Execute login
-app.post('/executeLogin', function (req, res) {
-	
-	//{\"email\":\"jason@gmail.com\",\"password\":\"test_pass\"}
-	
-	console.log("Login req received. Email: " + req.body.user.email);
-	
-	var sendR = '{\"email\":\"' + req.body.user.email + '\",\"password\":\"' + req.body.user.password + '\"}';
-	
-	var options = {
-		mode: 'text',
-		pythonPath: 'python3',
-		args: [sendR]
-	};
-	 
-	PythonShell.run('/scripts/repository/User_Auth.py', options, function (err, results) {	
-		if (err)
-		{
-			console.log("An error occured while trying to login: " + err);
->>>>>>> 79462f81e66553cd0373c37b0965263b5643b632
-			res.write("failed");
-			res.end();
-		}
-		else
-		{
-<<<<<<< HEAD
+		} else {
 			console.log("Datasets successfully fetched: " + results);
 			res.write(results);
 			res.end();
 		}
 	});
 });
-=======
+
+//Execute login
+app.post('/executeLogin', function (req, res) {
+
+	//{\"email\":\"jason@gmail.com\",\"password\":\"test_pass\"}
+
+	console.log("Login req received. Email: " + req.body.user.email);
+
+	var sendR = '{\"email\":\"' + req.body.user.email + '\",\"password\":\"' + req.body.user.password + '\"}';
+
+	var options = {
+		mode: 'text',
+		pythonPath: 'python3',
+		args: [sendR]
+	};
+
+	PythonShell.run('/scripts/repository/User_Auth.py', options, function (err, results) {
+		if (err){
+			console.log("An error occured while trying to login: " + err);
+			res.write("failed");
+			res.end();
+		} else {
 			console.log("User Login Returned. Output: " + results);
-			if (results == "True")
-			{
+			if(results == "True"){
 				res.write("success");
 				res.end();
-			}
-			else
-			{
+			} else {
 				res.write("failed");
 				res.end();
 			}
 		}
 	});
-})
->>>>>>> 79462f81e66553cd0373c37b0965263b5643b632
+
+});
